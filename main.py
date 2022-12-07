45de2d8fd8f9bb57367a71d26bd2dbda5bd11168
Kupo <jacobpmitchell@gmail.com>
2022-12-06 16:58:32 -0800

    Main Loop

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-from Utilities import Utilities
+from pymongo import MongoClient
 import Menu
 
-db = Utilities.startup()
+# Mongo setup
+cluster = "mongodb+srv://kupo:001657916579@termproject.7grkfgp.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(cluster)
+db = client.Term_Project
+
+# menu loop
+print("Welcome!")
+active_menu = True
+while active_menu:
+    tmp = Menu.io_Menu(db)
+    if tmp is 1:
+        active_menu = False
+    # catch any weird errors
+    if tmp is 2:
+        print("Wtf, this is redundant, how did you get here?")
+        active_menu = False
+print("Thank you!")
