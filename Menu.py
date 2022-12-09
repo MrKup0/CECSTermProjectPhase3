fb9ad0fecef2e2e20c1c547a3475c8fe9eacc1a1
MrKup0 <jacobpmitchell@gmail.com>
2022-12-09 09:16:13 -0800

    Changes

--- Menu.py
+++ Menu.py
@@ -20,25 +20,30 @@ def io_Menu(db):
     MenuMethods.create_key(db)
   # Create request
   elif option == 2:
+    MenuMethods.create_request(db)
 
   # Get employee room access
   elif option == 3:
     MenuMethods.list_employee_room_access(db)
-
   # Delete a key
   elif option == 4:
+    MenuMethods.delete_key(db)
 
   # Delete employee
   elif option == 5:
+    MenuMethods.delete_employee(db)
 
   # Add new door
   elif option == 6:
+    MenuMethods.add_door(db)
 
   # Change request
   elif option == 7:
+    MenuMethods.update_request(db)
 
   # Log Lost Key
   elif option == 8:
+    MenuMethods.lost_key_logged(db)
 
   # Log Room Access
   elif option == 9:
--- MenuMethods.py
+++ MenuMethods.py
@@ -36,7 +36,6 @@ def create_key(db):
                              'door_id': DBRef('doors', doors_list[random.randint(0, len(doors_list) - 1)]['_id'])})
         except Exeption as ex:
             return
-
 def list_employee_room_access(db):
     employees = db.employees
     print("Employees:")
@@ -49,7 +48,7 @@ def list_employee_room_access(db):
     tmp = []
     for i in matching_requests:
         tmp.append(i["_id"])
-    if not (len(tmp) > 0):
+    if (len(tmp) == 0):
         print("No keys have been issued for this employee")
         return
     matching_issued_keys = db.issued_keys.find({"request_id": {"$in": tmp}})
@@ -92,44 +91,45 @@ def list_employee_room_access(db):
 
 def delete_key(db):
     #Check to see if there is a key that is created
-    key = int(input("enter the key_id of the key: "))
-    rmp = []
-    for i in key:
-        rmp.append(i["_id"])
-    if not (len(rmp) > 0):
-        print("No keys have been issued for this employee")
-        return
-    matching_issued_keys = db.issued_keys.find({"request_id": {"$in": rmp}})
-  #Once all issued keys have been found delete them if they exsist
-      if (key in matching_issued_keys):
-        del key
-    else:
-        print("Key does not exsist please try again. ")
+    l_keys = list(db.keys.find())
+    for i in l_keys:
+        print("key_id: "+ i['_id'])
+    key = int(input("enter the key_id of the key you wish to remove: "))
+    matching_issued_keys = list(db.issued_keys.find({"key_id": key}))
+    request_ids = []
+    for i in matching_issued_keys:
+        request_ids.append(i['request_id'])
+    # Remove requests associated with those keys
+    db.requests.delete_many({'_id' : {'$in': request_ids}})
+    # Remove issued keys
+    db.issued_keys.delete_many({'key_id': key})
+    # Remove the key
+    db.keys.delete_one({'_id': key})
 
 def lost_key_logged(db):
-    def lost_key_logged(db):
-        # Find issued keys based on matching requests
-        lost_key = int(input("Enter the key_id for the lost key: "))
-        bad_emp = int(input("Enter your employee id: "))
-        # Find employee request
-        bad_emp_requests = list(db.requests.find({'employee_id': bad_emp}))
-        if len(bad_emp_requests) == 0:
-            print("Please double check that id")
-            return
-        useable_request = []
-        for i in bad_emp_requests:
-            useable_request.append(i["_id"])
-        # Find the issued key matching the request and key
-        try:
-            matching_issued_keys = db.issued_keys.update_one(
-                {"request_id": {"$in": useable_request}, "key_id": lost_key},
-                {'date_lost': datetime.datetime.now()}
-            )
-            print("key has been marked as lost, charging employee...")
-        except Exception as ex:
-            print("Could not resolve update")
-        current_emp_bal = list(db.employees.find({'_id': bad_emp}))
-        db.employees.update_one({'_id': bad_emp}, {'balance': current_emp_bal[0]['balance'] + 20})  
+    # Find issued keys based on matching requests
+    lost_key = int(input("Enter the key_id for the lost key: "))
+    bad_emp = int(input("Enter your employee id: "))
+    # Find employee request
+    bad_emp_requests = list(db.requests.find({'employee_id': bad_emp}))
+    if len(bad_emp_requests) == 0:
+        print("Please double check that id")
+        return
+    useable_request = []
+    for i in bad_emp_requests:
+        useable_request.append(i["_id"])
+    # Find the issued key matching the request and key
+    try:
+        matching_issued_keys = db.issued_keys.update_one(
+            {"request_id": {"$in": useable_request}, "key_id": lost_key},
+            {'date_lost': datetime.datetime.now()}
+        )
+        print("key has been marked as lost, charging employee...")
+    except Exception as ex:
+        print("Could not resolve update")
+    current_emp_bal = list(db.employees.find({'_id': bad_emp}))
+    db.employees.update_one({'_id': bad_emp}, {'balance': current_emp_bal[0]['balance'] + 20})
+
 def delete_employee(db):
   #Check to see if employee is there
   employee_id = int(input("Enter the employee_id: "))
@@ -243,3 +243,14 @@ def create_request(db):
                 'date_requested': datetime.now()
             })
     print('Request has been made! Good luck')
+
+def list_room_access(db):
+    # Prompt for rooms
+    rooms_cursor = db.rooms.find()
+    for i in rooms_cursor:
+        print("building code: " + i['building_name']+" room: " + i['_id'])
+    selected_room = input("Enter the room number")
+    # Find doors associated with room
+    # Find Keys asociated with doors
+    # Find Issued_keys associated with keys
+    # Find employee requests associated with issued keys
